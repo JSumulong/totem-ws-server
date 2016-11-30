a488a1185f97fcde9dcf6eb58182e0d687813183
Matt Grimm & Sam Hopkins <pair+grimmer20.hopkins571@devbootcamp.com>
2016-11-30 12:16:08 -0600

    Working websocket.

---
+++ server.js
@@ -0,0 +1,30 @@
+var server = require('http').createServer()
+  , url = require('url')
+  , WebSocketServer = require('ws').Server
+  , wss = new WebSocketServer({ server: server })
+  , express = require('express')
+  , app = express()
+  , uuid = require('uuid/v4')
+  , port = 4080;
+
+app.use(function (req, res) {
+  res.send({ msg: "hello" }); // change to variable locations once test passes
+});
+
+var totem = {}
+totem.broadcast = function(data) {
+  wss.clients.forEach(function each(client){
+    console.log(client);
+    client.send(JSON.stringify(data));
+  });
+}
+totem.onMessage = totem.broadcast;
+totem.onConnect = function connection(ws) {
+  ws.on('message', totem.onMessage);
+  ws.totemId = uuid();
+  ws.send(JSON.stringify({type: "newConnection", id: ws.totemId}))
+}
+
+wss.on('connection', totem.onConnect);
+server.on('request', app);
+server.listen(port, function () { console.log('Listening on ' + server.address().port) });
